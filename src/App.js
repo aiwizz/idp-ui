fd6ec2c6f4f6709363757dcb0dafe9b44ae58d2a
Desire Bah Bioh <aiwizz@Desires-MacBook-Air.local>
2024-09-11 00:46:08 -0400

    handle api keys in env vars

--- src/App.js
+++ src/App.js
@@ -8,41 +8,54 @@ import AccountPage from './components/AccountPage';
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import ResetPassword from './components/auth/ResetPassword';
 import Verify2FA from './components/auth/Verify2FA';
-import PaymentPage from './components/PaymentPage';  // Import PaymentPage
+import PaymentPage from './components/PaymentPage';  
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import axios from 'axios';
 
-const stripePromise = loadStripe('pk_test_51OvqmrJeGVIgSUHZ3BYL0P9QVXv1QfMFFXM0C220hxzij2YioVAgNxsihfPr2BV0YBz0P4wi9Cr3ElTLSP26mqao00our52zBA');
 
 function App() {
   const [uploadedFiles, setUploadedFiles] = useState([]);
-  const [fields, setFields] = useState([]); // State for managing fields
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // Track authentication status
+  const [fields, setFields] = useState([]);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
-  const [emailFor2FA, setEmailFor2FA] = useState(''); // Store email for 2FA verification
+  const [emailFor2FA, setEmailFor2FA] = useState('');
 
+  const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+  
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const checkTokenValidity = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          throw new Error("No token found");
+        }
 
-    if (location.pathname === '/reset_password' || location.pathname === '/verify_2fa') {
-      return; // Allow access to reset password and 2FA pages without being authenticated
-    }
+        // Make an API call to verify token
+        const response = await axios.get('http://127.0.0.1:5000/account', {
+          headers: { Authorization: `Bearer ${token}` }
+        });
 
-    if (token) {
-      setIsAuthenticated(true);
-      if (location.pathname === '/') {
-        navigate('/home');
-      }
-    } else {
-      setIsAuthenticated(false);
-      if (location.pathname !== '/') {
+        if (response.status === 200) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+          navigate('/');
+        }
+      } catch (error) {
+        console.error('Token is expired or invalid. Redirecting to login...');
+        setIsAuthenticated(false);
         navigate('/');
       }
-    }
-  }, [navigate, location]);
+    };
 
-  const disableBrowse = location.pathname === '/account'; // Disable "Browse Files" on the Account page
+    if (location.pathname !== '/' && location.pathname !== '/reset_password' && location.pathname !== '/verify_2fa') {
+      checkTokenValidity();
+    }
+  }, [location, navigate]);
+
+  const disableBrowse = location.pathname === '/account';
 
   return (
     <Box>
